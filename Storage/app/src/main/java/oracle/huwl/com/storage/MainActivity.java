2ad02b8e9ac89415e2ec660132d48101bcde5f90
KylinChen <kylinchen85@foxmail.com>
2017-07-05 20:33:44 +0800

    android storage

---
+++ Storage/app/src/main/java/oracle/huwl/com/storage/MainActivity.java
@@ -0,0 +1,30 @@
+package oracle.huwl.com.storage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void testSharePreference(View v){
+        startActivity(new Intent(this,SpActivity.class));
+    }
+    public void testInnerFile(View v){
+
+    }
+    public void testOutterFile(View v){
+
+    }
+    public void testDatabase(View v){
+
+    }
+    public void testNetwork(View v){
+
+    }
+}
